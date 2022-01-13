262cafd62ed09854d8b4013bf86a3a6729eafdb1
Mohamed BOURGA <keyserve.contact@gmail.com>
2022-01-13 18:50:00 +0100

    updated react app to use query service

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,19 +1,6 @@
-import axios from 'axios'
 import React from 'react'
 
-export default function CommentList({postId}) {
-
-    const [comments, setComments] = React.useState([])
-
-    const fetchData = async () => {
-        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
-
-        setComments(res.data)
-    }
-
-    React.useEffect(() => {
-        fetchData()
-    },[])
+export default function CommentList({ comments}) {
 
     const renderedComment = comments.map(comment => {
         return <li key={comment.id}>{comment.content}</li>
