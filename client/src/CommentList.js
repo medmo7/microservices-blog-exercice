391798d3e1446652670602d497dd28f9becbde5c
Mohamed BOURGA <keyserve.contact@gmail.com>
2022-01-15 22:08:47 +0100

    added event storing

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -3,7 +3,18 @@ import React from 'react'
 export default function CommentList({ comments}) {
 
     const renderedComment = comments.map(comment => {
-        return <li key={comment.id}>{comment.content}</li>
+        let content;
+        if (comment.status === 'approved') {
+            content = comment.content
+        }
+        if (comment.status === 'rejected') {
+            content = 'This comment has been rejected'
+        }
+        if (comment.status === 'pending') {
+            content = 'Content waiting moderation'
+        }
+
+        return <li key={comment.id}>{content}</li>
     })
 
     return (
