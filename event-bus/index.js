e35ed02ce4929f22044f73aa32bbfe3f40517c84
Mohamed BOURGA <keyserve.contact@gmail.com>
2022-01-26 09:39:27 +0100

    dev continued

--- event-bus/index.js
+++ event-bus/index.js
@@ -15,9 +15,9 @@ app.post('/events',(req, res) => {
     events.push(event);
 
     axios.post('http://posts-clusterip-srv:4000/events', event).catch(error => console.log('error in event bus 1', error))
-    // axios.post('http://localhost:4001/events', event).catch(error => console.log('error in event bus 2', error))
-    // axios.post('http://localhost:4002/events', event).catch(error => console.log('error in event bus 3', error))
-    // axios.post('http://localhost:4003/events', event).catch(error => console.log('error in event bus 4', error))
+    axios.post('http://comments-srv:4001/events', event).catch(error => console.log('error in event bus 2', error))
+    axios.post('http://query-srv:4002/events', event).catch(error => console.log('error in event bus 3', error))
+    axios.post('http://moderation-srv:4003/events', event).catch(error => console.log('error in event bus 4', error))
 
     res.send({ status: 'OK'});
 })
